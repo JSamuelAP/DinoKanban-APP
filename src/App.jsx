44d2007fc260d38c6b3d7b481038814e38dd6a6c
JSamuel <sp4619168@gmail.com>
2023-12-27 21:34:16 -0600

    feat: Route pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import { Typography } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+	Board,
+	Boards,
+	Error404,
+	LandingPage,
+	Login,
+	Profile,
+	Signup,
+} from "./pages/index.js";
 
 function App() {
 	return (
 		<>
-			<Typography variant="h1">hola</Typography>
+			<Router>
+				<Routes>
+					<Route path="/" element={<LandingPage />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/boards" element={<Boards />} />
+					<Route path="/boards/:id" element={<Board />} />
+					<Route path="/profile" element={<Profile />} />
+					<Route path="*" element={<Error404 />} />
+				</Routes>
+			</Router>
 		</>
 	);
 }
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Board from "./Board.jsx";
+import Boards from "./Boards.jsx";
+import Error404 from "./Error404.jsx";
+import LandingPage from "./LandingPage.jsx";
+import Login from "./Login.jsx";
+import Profile from "./Profile";
+import Signup from "./Signup.jsx";
+
+export { Board, Boards, Error404, LandingPage, Login, Profile, Signup };
