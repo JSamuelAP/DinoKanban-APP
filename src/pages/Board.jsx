4862cc4e752cc809941567e0a535008a7bc218a8
JSamuel <sp4619168@gmail.com>
2024-01-13 19:56:43 -0600

    feat: Show 404 message for board not found

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
-import { Box, Divider, Grid, Typography } from "@mui/material";
+import { Box, Button, Divider, Grid, Typography } from "@mui/material";
 import CalendarIcon from "@mui/icons-material/CalendarMonth";
-import { useParams } from "react-router-dom";
+import { Link as RouterLink, useParams } from "react-router-dom";
 
 import { Layout, CardBoardMenu, BoardList, EditableText } from "../components/";
 import dayjs from "../helpers/dayjs.js";
 
 const board = {
-	id: "1",
-	name: "Board name",
-	createdAt: "2024-01-01T19:15:00.782Z",
-	updatedAt: "2024-01-04T19:15:00.782Z",
-	favorite: true,
+	// id: "1",
+	// name: "Board name",
+	// createdAt: "2024-01-01T19:15:00.782Z",
+	// updatedAt: "2024-01-04T19:15:00.782Z",
+	// favorite: true,
 };
 
 const tasks = {
@@ -137,56 +137,89 @@ const Board = () => {
 		<>
 			<Layout>
 				<Box component="main" mt={4} mb={8}>
-					<Box component="section">
-						<Box
-							display="flex"
-							justifyContent="space-between"
-							alignItems="start"
-						>
-							<EditableText
-								textNode={
-									<Typography component="h1" variant="h4">
-										{board.name}
-									</Typography>
-								}
-								isEditMode={editMode}
-								board={board}
-								handleBlur={() => setEditMode(false)}
-								variant="board-page"
-							/>
-							<CardBoardMenu
-								board={board}
-								handleEdit={() => setEditMode(true)}
-							/>
-						</Box>
-						<Typography
-							component="time"
-							dateTime={dayjs(board.createdAt).format("YYYY-MM-DD")}
-							variant="body2"
-							color="gray"
-							display="inline-block"
-							mb={2}
-						>
-							<CalendarIcon
-								fontSize="small"
-								sx={{ verticalAlign: "top", mr: 0.5 }}
-							/>
-							Created at {dayjs(board.createdAt).format("dddd, MMMM DD, YYYY")}
-						</Typography>
-						<Divider />
-					</Box>
-					<Box component="section" mt={4}>
-						<Grid container spacing={{ xs: 2, md: 3 }}>
-							{lists.map(({ name, title }) => (
-								<Grid item xs={12} sm={6} md={3} key={name}>
-									<BoardList
-										list={{ name, title, tasks: tasks[name] }}
-										board={board.id}
+					{board.id ? (
+						<>
+							<Box component="section">
+								<Box
+									display="flex"
+									justifyContent="space-between"
+									alignItems="start"
+								>
+									<EditableText
+										textNode={
+											<Typography component="h1" variant="h4">
+												{board.name}
+											</Typography>
+										}
+										isEditMode={editMode}
+										board={board}
+										handleBlur={() => setEditMode(false)}
+										variant="board-page"
 									/>
+									<CardBoardMenu
+										board={board}
+										handleEdit={() => setEditMode(true)}
+									/>
+								</Box>
+								<Typography
+									component="time"
+									dateTime={dayjs(board.createdAt).format("YYYY-MM-DD")}
+									variant="body2"
+									color="gray"
+									display="inline-block"
+									mb={2}
+								>
+									<CalendarIcon
+										fontSize="small"
+										sx={{ verticalAlign: "top", mr: 0.5 }}
+									/>
+									Created at{" "}
+									{dayjs(board.createdAt).format("dddd, MMMM DD, YYYY")}
+								</Typography>
+								<Divider />
+							</Box>
+							<Box component="section" mt={4}>
+								<Grid container spacing={{ xs: 2, md: 3 }}>
+									{lists.map(({ name, title }) => (
+										<Grid item xs={12} sm={6} md={3} key={name}>
+											<BoardList
+												list={{ name, title, tasks: tasks[name] }}
+												board={board.id}
+											/>
+										</Grid>
+									))}
 								</Grid>
-							))}
-						</Grid>
-					</Box>
+							</Box>
+						</>
+					) : (
+						<>
+							<Typography component="h1" textAlign="center" mt={8}>
+								<Typography
+									component="span"
+									fontWeight={700}
+									fontSize="12rem"
+									lineHeight={1}
+									color="primary"
+									display="block"
+								>
+									404
+								</Typography>
+								<Typography component="span" variant="h5" display="block">
+									The board you are looking for cannot be found
+								</Typography>
+							</Typography>
+							<Box textAlign="center" mt={8}>
+								<Button
+									LinkComponent={RouterLink}
+									to="/boards"
+									variant="contained"
+									size="large"
+								>
+									Go Boards page
+								</Button>
+							</Box>
+						</>
+					)}
 				</Box>
 			</Layout>
 		</>
