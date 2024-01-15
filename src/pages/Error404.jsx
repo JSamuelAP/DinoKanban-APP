32e33e7b892dbe305104f066b8e0c12ca0c721dd
JSamuel <sp4619168@gmail.com>
2024-01-15 15:00:57 -0600

    feat: Create 404 page for not found pages

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+
+import { Layout } from "../components/";
+
 const Error404 = () => {
-	return <div>Error404</div>;
+	const [isAuth, setIsAuth] = useState(false);
+
+	return (
+		<>
+			<Layout>
+				<Typography component="h1" textAlign="center" mt={8}>
+					<Typography
+						component="span"
+						fontWeight={700}
+						fontSize="12rem"
+						lineHeight={1}
+						color="primary"
+						display="block"
+					>
+						404
+					</Typography>
+					<Typography component="span" variant="h5" display="block">
+						The page you are looking for cannot be found
+					</Typography>
+				</Typography>
+				<Box textAlign="center" mt={8}>
+					<Button
+						LinkComponent={RouterLink}
+						to={isAuth ? "/boards" : "/"}
+						variant="contained"
+						size="large"
+					>
+						{isAuth ? "Go Boards page" : "Go Home page"}
+					</Button>
+				</Box>
+			</Layout>
+		</>
+	);
 };
 
 export default Error404;
